602d80257320c73ea3f00339dd76e0ae58cba3e1
danielebarell <dan.barell@gmail.com>
2020-04-08 20:21:27 +0200

    dan: version label added

--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@
 import './style.css';
 
 // Write TypeScript code!
+const version = '0.0.1';
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>Ciao Gabriele!</h1>
 <footer>
   <a href="https://medium.com/@creativomoc/stackblitz-with-github-and-firebase-fastest-way-to-develop-version-control-build-and-deploy-a-73c311eb07ea" target="blank"> → tutorial</a>
+  <aside>${version}</aside>
 </footer>`;
